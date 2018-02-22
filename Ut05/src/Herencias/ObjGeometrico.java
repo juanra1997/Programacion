d5e071ab9ca4a5f2c37fafc2e212cd50e56f1499
Unknown <juanrainmortal22@yahoo.com>
2018-02-22 10:33:34 +0100

    Todo

---
+++ Ut05/src/Herencias/ObjGeometrico.java
@@ -0,0 +1,19 @@
+package Herencias;
+
+public class ObjGeometrico {
+
+	protected double px;
+	protected double py;
+	
+	public ObjGeometrico(double x, double y) {
+		
+	}
+	
+	public ObjGeometrico() {
+		
+	}
+	
+	public void imprimirCentro() {
+		
+	}
+}
