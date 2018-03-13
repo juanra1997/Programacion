86d043a0f5cb23060f26cbbe13184096c329906b
Unknown <juanrainmortal22@yahoo.com>
2018-03-13 13:14:08 +0100

    +

--- Ut05/src/Herencias/Lavadora.java
+++ Ut05/src/Herencias/Lavadora.java
@@ -1,5 +1,27 @@
 package Herencias;
 
-public class Lavadora {
+public class Lavadora extends Electrodomestico {
 
+	private int carga;
+	
+	public Lavadora() {
+		super();
+		this.carga=5;
+	}
+	
+	public Lavadora(double precio, double peso) {
+		super(precio, peso);
+		this.carga=5;
+	}
+	
+	public Lavadora(double precio, String color, char consumo, double peso, int carga) {
+		super(precio, color, consumo, peso);
+		this.carga=carga;
+	}
+	
+	public int getCarga() {
+		return this.carga;
+	}
+	
+	
 }
