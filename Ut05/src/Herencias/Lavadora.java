8ad73e533fb353cfab1efac9bb239ad9d80f06e7
Unknown <juanrainmortal22@yahoo.com>
2018-03-14 11:29:14 +0100

    +

--- Ut05/src/Herencias/Lavadora.java
+++ Ut05/src/Herencias/Lavadora.java
@@ -23,5 +23,33 @@ public class Lavadora extends Electrodomestico {
 		return this.carga;
 	}
 	
-	
+	public double precioFinal() {
+		double precio=this.getPrecioBase();
+		if(this.getConsumoEnergetico()=='A') {
+			precio+=100;
+		} else if(this.getConsumoEnergetico()=='B') {
+			precio+=80;
+		} else if(this.getConsumoEnergetico()=='C') {
+			precio+=60;
+		} else if(this.getConsumoEnergetico()=='D') {
+			precio+=50;
+		} else if(this.getConsumoEnergetico()=='E') {
+			precio+=30;
+		} else if(this.getConsumoEnergetico()=='F') {
+			precio+=10;
+		}
+		if(this.getPeso()<20) {
+			precio+=10;
+		} else if(this.getPeso()>=20&&this.getPeso()<50) {
+			precio+=50;
+		} else if(this.getPeso()>=50&&this.getPeso()<80) {
+			precio+=80;
+		} else if(this.getPeso()>=80) {
+			precio+=100;
+		}
+		if(this.carga>30) {
+			precio+=50;
+		}
+		return precio;
+	}
 }
